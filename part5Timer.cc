f6fc36f69a32d59dce94a0544b5b2be8ab8e24a4
Chan Ingrid Ka-Lam <c0chanin@cdf.toronto.edu>
2014-09-26 22:56:00 -0400

    added timer for part 5 experiments

---
+++ part5Timer.cc
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h> 
+#include <time.h> 
+
+int main(int argc, char ** argv) {
+	time_t before;
+	time_t after;
+	
+  	time(&before);
+   	system("csv2heapfile bigCsv heapfile 4000");
+	time(&after);
+
+	printf("Time taken to process csv2heapfile: %lu\n", (after - before));
+}
